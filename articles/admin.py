d8a6e1b311635c2fda0e0f8b26d3e24a70c215dc
Joseph Victor Zammit <jvzammit@gmail.com>
2021-05-06 16:34:54 +0200

    Applies isort+black

--- articles/admin.py
+++ articles/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+
 from articles.models import Article
 
 admin.site.register(Article)
--- articles/tests/test_models.py
+++ articles/tests/test_models.py
@@ -1,11 +1,9 @@
-
 from django.test import TestCase
 
 from articles.tests.factories import ArticleFactory
 
 
 class ArticleTest(TestCase):
-
     def test_str(self):
         article = ArticleFactory(title="Test Title")
         self.assertEqual(str(article), "Test Title")
