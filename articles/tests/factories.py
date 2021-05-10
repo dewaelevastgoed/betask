b041d26356b6759317a59c8b48151aba4fcb3097
Wajahat <wajahatahmad056@gmail.com>
2021-05-10 11:08:30 +0500

    added test cases for filtering article and content

--- articles/tests/factories.py
+++ articles/tests/factories.py
@@ -6,6 +6,7 @@ from articles.models import Article
 class ArticleFactory(factory.django.DjangoModelFactory):
     title = factory.Sequence(lambda n: f"Article {n}")
     slug = factory.Sequence(lambda n: f"article-{n}")
+    content = factory.Sequence(lambda n: f"content-{n}")
 
     class Meta:
         model = Article
