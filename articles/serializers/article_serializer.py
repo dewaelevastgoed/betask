36118db519bad16e34b0b617fc2fb3291b26dc4d
Krishna Manohar <krishna.m@cawstudios.com>
2021-05-16 17:24:20 +0530

    Fix sorts with black

--- articles/serializers/article_serializer.py
+++ articles/serializers/article_serializer.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 
 from articles.models import Article, Tag
 
+
 class ArticleSerializer(serializers.ModelSerializer):
     tags = serializers.PrimaryKeyRelatedField(queryset=Tag.objects.all(), many=True)
 
