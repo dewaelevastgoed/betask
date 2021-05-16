d4a12f3c811850c12baf05ef7ce5bcaea727cbfb
Krishna Manohar <krishna.m@cawstudios.com>
2021-05-16 17:23:49 +0530

    Refactored Serializers structure and removed old Views file

--- articles/serializers/article_serializer.py
+++ articles/serializers/article_serializer.py
@@ -2,13 +2,6 @@ from rest_framework import serializers
 
 from articles.models import Article, Tag
 
-
-class TagSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Tag
-        fields = "__all__"
-
-
 class ArticleSerializer(serializers.ModelSerializer):
     tags = serializers.PrimaryKeyRelatedField(queryset=Tag.objects.all(), many=True)
 
