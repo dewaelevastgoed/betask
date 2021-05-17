54973fc23317fd06295484fe8cf781704c901c39
SrinivasK438 <shrinivaskalyani05@gmail.com>
2021-05-17 16:27:19 +0530

    Feat: Article list filtering and sorting.

--- articles/views.py
+++ articles/views.py
@@ -1,4 +1,6 @@
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import generics
+from rest_framework.filters import OrderingFilter
 
 from articles.models import Article
 from articles.serializers import ArticleSerializer
@@ -7,6 +9,9 @@ from articles.serializers import ArticleSerializer
 class ArticleListCreateAPIView(generics.ListCreateAPIView):
     queryset = Article.objects.all()
     serializer_class = ArticleSerializer
+    filter_backends = (DjangoFilterBackend, OrderingFilter)
+    filterset_fields = ('title', 'content')
+    ordering_fields = ('title', 'created_at')
 
 
 class ArticleDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
